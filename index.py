2b196d116f309f58bb4459fd9cf55aa751ee49d4
james23216 <jhmun23216@gmail.com>
2022-06-23 17:53:24 -0700

    aa

--- index.py
+++ index.py
@@ -17,7 +17,7 @@ import os
 import re
 
 chrome_options = webdriver.ChromeOptions()
-#options.headless = True
+#options.headless = True 
 chrome_options.add_argument("--window-size=1920,1080")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-gpu")
